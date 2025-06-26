13468e9901e98ca64c4f2df47ddf4f77a58ff744
Ananya Giri <ananyagiri257@gmail.com>
2025-06-26 12:58:15 +0530

    Fix: added user-agent header for newsapi

--- 01newssum/src/api/newsAPI.js
+++ 01newssum/src/api/newsAPI.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
 const API_KEY = import.meta.env.VITE_NEWS_API_KEY
-
 const BASE_URL = 'https://newsapi.org/v2/top-headlines'
 
 export const getNewsByCategory = async (category = 'general') => {
@@ -9,17 +8,17 @@ export const getNewsByCategory = async (category = 'general') => {
     const response = await axios.get(BASE_URL, {
       params: {
         category,
-        country: 'us', // India
+        country: 'us', // 'in' sometimes returns empty
         apiKey: API_KEY,
         pageSize: 9
+      },
+      headers: {
+        'User-Agent': 'Briefly-NewsApp/1.0'
       }
     })
     return response.data.articles
   } catch (error) {
-    console.error('Error fetching news:', error)
-
-    console.log('NewsAPI response:', response.data)
-
+    console.error('Error fetching news:', error.response?.data || error.message)
     return []
   }
 }
